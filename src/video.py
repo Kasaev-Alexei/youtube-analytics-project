3c08b9bf2b46b05f15950b135aa3f1f995e6dd1e
Alexei Kasaev <kasaev_97@mail.ru>
2024-01-15 00:11:17 +0400

    add code

--- src/video.py
+++ src/video.py
@@ -1,35 +1,59 @@
-import os
-
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
+import os
+import typing
 
 load_dotenv()
 
 
 class Video:
-    def __init__(self, video_id):
-        self.video_id = video_id
-        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                          id=self.video_id).execute()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+    API_KEY: str | None = os.getenv('YT_API_KEY')
 
-    def __str__(self):
-        return f"{self.title}"
+    def __init__(self, video_id: str):
+        self.video_id = video_id
+        self.title = None
+        self.video_url = None
+        self.view_count = None
+        self.like_count = None
+        self.error_message = None
+
+        try:
+            video_info = self.fetch_video_info()
+            self.title = video_info["video_title"]
+            self.video_url = video_info["video_url"]
+            self.view_count = video_info["view_count"]
+            self.like_count = video_info["like_count"]
+        except Exception as e:
+            self.error_message = str(e)
 
     @classmethod
-    def get_service(cls):
-        api_key = os.getenv("YT_API_KEY")
-        cls.youtube = build('youtube', 'v3', developerKey=api_key)
-        return build('youtube', 'v3', developerKey=api_key)
+    def get_service(cls) -> typing.Any:
+        """Возвращает объект для работы с API YouTube."""
+        service = build('youtube', 'v3', developerKey=cls.API_KEY)
+        return service
+
+    def fetch_video_info(self) -> dict:
+        """Получает информацию о видео с помощью YouTube API."""
+        youtube = self.get_service()
+        video_response: object = (
+            youtube.videos().list(part="snippet,statistics,contentDetails,topicDetails", id=self.video_id).execute()
+        )
+        video_title: str = video_response["items"][0]["snippet"]["title"]
+        video_url: str = "https://youtu.be/" + self.video_id
+        view_count: int = video_response["items"][0]["statistics"]["viewCount"]
+        like_count: int = video_response["items"][0]["statistics"]["likeCount"]
+        return {
+            "video_title": video_title,
+            "view_count": view_count,
+            "like_count": like_count,
+            "video_url": video_url,
+        }
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-
-    def __str__(self):
-        return f"{self.title}"
