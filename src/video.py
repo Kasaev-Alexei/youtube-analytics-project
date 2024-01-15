e27c1321e9ac413ae0bf70d4605b2c92da833e0f
Alexei Kasaev <kasaev_97@mail.ru>
2024-01-15 23:46:52 +0400

    fix

--- src/video.py
+++ src/video.py
@@ -1,59 +1,39 @@
-from googleapiclient.discovery import build
-from dotenv import load_dotenv
-import os
-import typing
+from typing import Any
 
-load_dotenv()
+from src.channel import Channel
 
 
-class Video:
-    API_KEY: str | None = os.getenv('YT_API_KEY')
-
-    def __init__(self, video_id: str):
-        self.video_id = video_id
-        self.title = None
-        self.video_url = None
-        self.view_count = None
-        self.like_count = None
-        self.error_message = None
-
+class Video(Channel):
+    def __init__(self, id_video: Any) -> None:
         try:
-            video_info = self.fetch_video_info()
-            self.title = video_info["video_title"]
-            self.video_url = video_info["video_url"]
-            self.view_count = video_info["view_count"]
-            self.like_count = video_info["like_count"]
-        except Exception as e:
-            self.error_message = str(e)
+            self.id_video = id_video
+            youtube = self.get_service()
+            self.video = (youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.id_video)
+                          .execute())
+            self.title = self.video['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + self.id_video
+            self.number_view = self.video['items'][0]['statistics']['viewCount']
+            self.number_likes = self.video['items'][0]['statistics']['likeCount']
+            self.duration = self.video["items"][0]['contentDetails']['duration']
+            self.likes_count = self.video["items"][0]["statistics"]["likeCount"]
+        except Exception:
+            self.id_video = id_video
+            youtube = None
+            self.video = None
+            self.title = None
+            self.url = None
+            self.number_view = None
+            self.number_likes = None
+            self.duration = None
+            self.likes_count = None
 
-    @classmethod
-    def get_service(cls) -> typing.Any:
-        """Возвращает объект для работы с API YouTube."""
-        service = build('youtube', 'v3', developerKey=cls.API_KEY)
-        return service
-
-    def fetch_video_info(self) -> dict:
-        """Получает информацию о видео с помощью YouTube API."""
-        youtube = self.get_service()
-        video_response: object = (
-            youtube.videos().list(part="snippet,statistics,contentDetails,topicDetails", id=self.video_id).execute()
-        )
-        video_title: str = video_response["items"][0]["snippet"]["title"]
-        video_url: str = "https://youtu.be/" + self.video_id
-        view_count: int = video_response["items"][0]["statistics"]["viewCount"]
-        like_count: int = video_response["items"][0]["statistics"]["likeCount"]
-        return {
-            "video_title": video_title,
-            "view_count": view_count,
-            "like_count": like_count,
-            "video_url": video_url,
-        }
-
-    def __str__(self) -> str:
-        return self.title
+        def __str__(self):
+            return f'{self.title}'
 
 
 class PLVideo(Video):
-    def __init__(self, video_id, playlist_id):
-        super().__init__(video_id)
-        self.playlist_id = playlist_id
+    def __init__(self, id_video: Any, play_list_id: Any) -> None:
+        super().__init__(id_video)
+        youtube = self.get_service()
+        self.play_list_id = (youtube.playlistItems().list(playlistId=play_list_id, part='contentDetails',
+                                                          maxResults=50).execute())
